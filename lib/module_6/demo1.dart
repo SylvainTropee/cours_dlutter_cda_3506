0e0c6b8c314fd4fdfe13079abb86e920ef8a2794
stropee <stropee@campus-eni.fr>
2026-06-24 11:25:46 +0200

    module 6 demo 1

---
+++ lib/module_6/demo1.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
+      home: const Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: FormPage());
+  }
+}
+
+class FormPage extends StatefulWidget {
+  const FormPage({super.key});
+
+  @override
+  State<FormPage> createState() => _FormPageState();
+}
+
+class _FormPageState extends State<FormPage> {
+  GlobalKey<FormState> _key = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _key,
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: "Nom",
+                hintText: "Veuillez saisir votre nom...",
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 16),
+            TextFormField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: "Age",
+                hintText: "Veuillez saisir votre age...",
+                border: OutlineInputBorder(),
+              ),
+            ),
+            DropdownButtonFormField<String>(
+              onChanged: (value) {
+                print(value);
+              },
+              initialValue: "",
+              items: [
+                DropdownMenuItem(
+                  child: Text("Choisir une glace"),
+                  value: "",
+                  enabled: false,
+                ),
+                DropdownMenuItem(
+                  child: Text("Snickers glacé"),
+                  value: "snicker",
+                ),
+                DropdownMenuItem(child: Text("Vienneta"), value: "vienneta"),
+                DropdownMenuItem(
+                  child: Text("Haggen daz - Macadamia"),
+                  value: "haggen",
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Checkbox(value: true, onChanged: (value) {}),
+                Text("La <Form> ?"),
+              ],
+            ),
+            RadioGroup(
+              groupValue: "eat",
+              onChanged: (value){},
+              child: Column(
+                children: [
+                  RadioListTile(value: "takeaway", title: Text("A emporter")),
+                  RadioListTile(value: "onplace", title: Text("Sur place")),
+                ],
+              ),
+            ),
+            OutlinedButton(onPressed: (){}, child: Text("Save"))
+          ],
+        ),
+      ),
+    );
+  }
+}
